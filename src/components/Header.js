01bc85cde9017c512b47c4bca9c39a31ec7e7bee
kyoungmin Lee <stephen4951@gmail.com>
2023-12-19 05:27:40 +0900

    login 모달 대충 완성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,57 @@
-import React, { useEffect, useState } from "react";
-import serachIcon from "../assets/img/search.svg";
-import "./Header.scss";
-import SignModal from "./SignModal";
+import { AppBar, Box, Button, InputBase, Toolbar } from '@mui/material';
+import React, { useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled } from '@mui/material/styles';
+import './Header.scss';
+import SignModal from './SignModal';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Header = () => {
-  const [signInTabOn, setSignInTabOn] = useState(false);
-  const [signUpTabOn, setSignUpTabOn] = useState(false);
 
-  const handleSignTabClick = (signIn) => {
-    if (signIn) {
-      setSignInTabOn(!signInTabOn);
-      setSignUpTabOn(false);
-    } else {
-      setSignUpTabOn(!signUpTabOn);
-      setSignInTabOn(false);
-    }
+const Header = () => {  
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  
+  const handleClose = () => {
+    setOpen(false);
   };
 
-  return (
-    <>
-      <div className="header">
-        <h2 className="logo">MarKeep</h2>
-        <div className="input-box">
-          <input className="search" placeholder="검색어를 입력하세요." />
-          <img className="icon" alt="Icon" src={serachIcon} />
-        </div>
-        <ul className="sign-nav">
-          <li onClick={() => handleSignTabClick(true)}>Sign In</li>
-          <li onClick={() => handleSignTabClick(false)}>Sign Up</li>
-        </ul>
-      </div>
-      {signInTabOn && <SignModal>signIn</SignModal>}
-      {signUpTabOn && <SignModal>signUp</SignModal>}
-    </>
-  );
-};
 
-export default Header;
+  const StyledLogo = styled(Box)`
+  && {
+    margin: 1rem 2rem;
+    font-family: Nunito-B;
+    font-size: 2rem;
+    background: linear-gradient(90deg, #6ecbf5, #c252e1);
+    -webkit-background-clip: text;
+    color: transparent;
+    flex-grow: 0.1
+  }
+  `;
+
+  return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position='static' color='primary' sx={{background: '#2a2356', outline: 'none'}}>
+          <Toolbar sx={{justifyContent: 'flex-start'}}>
+              <StyledLogo>
+                Markeep
+              </StyledLogo>
+              <div className='input-box'>
+                <input type='text' id='search' placeholder='검색어를 입력해 주세요.' />
+                <a className='search-img'></a>
+              </div>
+              <div className='button-group'>
+                <Button size='large' color="inherit" onClick={handleOpen}>Sign Up</Button>
+                <Button size='large' color="inherit">Sign In</Button>
+                <SignModal open={open} handleClose={handleClose} />
+              </div>
+            </Toolbar>
+        </AppBar>
+      </Box>
+  )
+}
+
+export default Header
