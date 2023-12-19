b91ee86591e147de38ee4282674f2647bf7fd036
kyoungmin <stephen4951@gmail.com>
2023-12-19 17:15:50 +0900

    로그인 회원가입 모달 대충 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,52 +6,72 @@ import './Header.scss';
 import SignModal from './SignModal';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-
-const Header = () => {  
-
+const Header = () => {
   const [open, setOpen] = useState(false);
+  const [openStatus, setOpenStatus] = useState({ open: false, value: null });
 
-  const handleOpen = () => {
-    setOpen(true);
+  const handleOpen = (e) => {
+    console.log(e.target.textContent);
+    setOpenStatus({ open: true, value: e.target.textContent });
   };
-  
+
   const handleClose = () => {
-    setOpen(false);
+    setOpenStatus({ open: false, value: null });
   };
-
 
   const StyledLogo = styled(Box)`
-  && {
-    margin: 1rem 2rem;
-    font-family: Nunito-B;
-    font-size: 2rem;
-    background: linear-gradient(90deg, #6ecbf5, #c252e1);
-    -webkit-background-clip: text;
-    color: transparent;
-    flex-grow: 0.1
-  }
+    && {
+      margin: 1rem 2rem;
+      font-family: Nunito-B;
+      font-size: 2rem;
+      background: linear-gradient(90deg, #6ecbf5, #c252e1);
+      -webkit-background-clip: text;
+      color: transparent;
+      flex-grow: 0.1;
+    }
   `;
 
   return (
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position='static' color='primary' sx={{background: '#2a2356', outline: 'none'}}>
-          <Toolbar sx={{justifyContent: 'flex-start'}}>
-              <StyledLogo>
-                Markeep
-              </StyledLogo>
-              <div className='input-box'>
-                <input type='text' id='search' placeholder='검색어를 입력해 주세요.' />
-                <a className='search-img'></a>
-              </div>
-              <div className='button-group'>
-                <Button size='large' color="inherit" onClick={handleOpen}>Sign Up</Button>
-                <Button size='large' color="inherit">Sign In</Button>
-                <SignModal open={open} handleClose={handleClose} />
-              </div>
-            </Toolbar>
-        </AppBar>
-      </Box>
-  )
-}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position='static'
+        color='primary'
+        sx={{ background: '#2a2356', outline: 'none' }}
+      >
+        <Toolbar sx={{ justifyContent: 'flex-start' }}>
+          <StyledLogo>Markeep</StyledLogo>
+          <div className='input-box'>
+            <input
+              type='text'
+              id='search'
+              placeholder='검색어를 입력해 주세요.'
+            />
+            <a className='search-img'></a>
+          </div>
+          <div className='button-group'>
+            <Button
+              size='large'
+              color='inherit'
+              onClick={handleOpen}
+            >
+              Sign In
+            </Button>
+            <Button
+              size='large'
+              color='inherit'
+              onClick={handleOpen}
+            >
+              Sign Up
+            </Button>
+            <SignModal
+              status={openStatus}
+              handleClose={handleClose}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/SignModal.js
+++ src/components/SignModal.js
@@ -1,217 +1,88 @@
-import React, { useState } from "react";
-import "./SignModal.scss";
-import SnsLogin from "./login/SnsLogin";
-import { Dialog, Tabs, Tab, Box, Typography, TextField, Button, Checkbox, FormControlLabel, Grid, InputAdornment, Link } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import './SignModal.scss';
+import SnsLogin from './login/SnsLogin';
+import {
+  Dialog,
+  Tabs,
+  Tab,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  InputAdornment,
+  Link,
+  Divider,
+} from '@mui/material';
 import { styled } from '@mui/material/styles';
 import EmailIcon from '@mui/icons-material/Email';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Modal_Login from './ModalLogin';
+import ModalLogin from './ModalLogin';
+import ModalJoin from './ModalJoin';
 
-const SignModal = ({ open, handleClose }) => {
-
-
-
+const SignModal = ({ status, handleClose }) => {
+  console.log('status: ', status);
   const [value, setValue] = useState('signIn');
+  console.log('useState value: ', value);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  useEffect(() => {
+    console.log('useEffect status: ', status);
+    status.value === 'Sign In' ? setValue('signIn') : setValue('signUp');
+  }, [status.value]);
+
   return (
-    <Dialog open={open} onClose={handleClose} PaperProps={{
-      sx: {
-        background: '#413a6f',
-        borderRadius: '30px'
-      }
-    }}>
-    <Tabs value={value} onChange={handleChange} variant='fullWidth'
-      sx={{
-        marginBottom: '2em',
-        '& .MuiTabs-indicator': {
-          backgroundColor: 'white', // 원하는 색상 코드로 변경
-        }
+    <Dialog
+      open={status.open}
+      onClose={handleClose}
+      PaperProps={{
+        sx: {
+          background: '#413a6f',
+          borderRadius: '30px',
+        },
       }}
     >
-      <Tab label="Sign In" value="signIn"
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant='fullWidth'
         sx={{
-          '&.Mui-selected': { color: '#6ecbf5' },  // 선택됐을 때의 색상
-          '&.Mui-focusVisible': { backgroundColor: 'rgba(100, 100, 100, 0.2)' },  // 포커스 됐을 때의 배경색
+          marginBottom: '2em',
+          '& .MuiTabs-indicator': {
+            backgroundColor: 'white', // 원하는 색상 코드로 변경
+          },
         }}
-      />
-      <Tab label="Sign Up" value="signUp" 
-        sx={{
-          '&.Mui-selected': { color: '#6ecbf5' },  // 선택됐을 때의 색상
-          '&.Mui-focusVisible': { backgroundColor: 'rgba(100, 100, 100, 0.2)' },  // 포커스 됐을 때의 배경색
-        }}
-      />
-    </Tabs>
-    {value === 'signIn' && (
-      <div className="form-wrap">
-        <form noValidate autoComplete="false">
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                placeholder="Account"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <EmailIcon sx={{color: 'gray'}} />
-                    </InputAdornment>
-                  ),
-                  style: {
-                    border: '1px solid #363636',
-                    color: "lightgray",
-                    width: '80%',
-                    margin: 'auto',
-                    borderRadius: '20px',
-                  },
-        
-                }}
-                sx={{
-                  '& .MuiOutlinedInput-root': {
-                    '&.Mui-focused fieldset': {
-                      borderColor: 'lightgray', // 포커스됐을 때의 테두리 색상 변경
-                    },
-                  },
-                }}
-              />
-            </Grid>
-        
-            <Grid item xs={12}>
-              <TextField
-                type="password"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                placeholder="Password"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <LockOutlinedIcon sx={{color: 'gray'}} />
-                    </InputAdornment>
-                  ),
-                  style: {
-                    border: '1px solid #363636',
-                    color: "lightgray",
-                    width: '80%',
-                    margin: 'auto',
-                    borderRadius: '20px'
-                  }
-                }}
-              />
-            </Grid>
-          </Grid>
-          <FormControlLabel control={<Checkbox sx={{
-                color: 'gray', // 체크되지 않았을 때의 색상
-                marginLeft: '4em',
-                '&.Mui-checked': {
-                  color: 'lightGray', // 체크됐을 때의 색상
-                },
-              }}
-        />} label="Remember"
+      >
+        <Tab
+          label='Sign In'
+          value='signIn'
           sx={{
-                '& .MuiTypography-body1': { // MUI v5 기준 Typography 스타일 클래스
-                  fontSize: '0.875rem', // 폰트 크기 조정
-                  color: 'lightGray', // 폰트 색상 조정
-                }
-              }}
-          />
-          <Link href='#' color={'lightGray'} underline="hover" sx={{marginLeft: '15em'}}>Forgot Password?</Link>
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            sx={{width: '50%', borderRadius: '30px', margin: '2em 10em' }}>Sign In
-          </Button>
-        </form>
-        <Box sx={{ margin: '3em'}}>
-          <Typography variant="h6" align="center">Don't have an account? Sign Up Now!</Typography>
-        </Box>
-      </div>
-    )}
-
-
-
-
-
-
-
-    {value === 'signUp' && (
-      <form noValidate autoComplete="false">
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              fullWidth
-              placeholder="Account"
-              margin="normal"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <EmailIcon />
-                  </InputAdornment>
-                ),      
-                style: {
-                  width: '80%',
-                  margin: 'auto',
-                  borderRadius: '20px',
-                  
-                }
-              }}
-            />
-          </Grid>
-        
-          <Grid item xs={12}>
-            <TextField 
-              type="password" 
-              variant="outlined" 
-              fullWidth
-              placeholder="Password"
-              margin="normal"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LockOutlinedIcon />
-                  </InputAdornment>
-                ),
-                style: {
-                  width: '80%',
-                  margin: 'auto',
-                  borderRadius: '20px'
-                }
-              }}
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <TextField 
-              type="password" 
-              variant="outlined" 
-              fullWidth
-              placeholder="Password Confirm"
-              margin="normal"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LockOutlinedIcon />
-                  </InputAdornment>
-                ),
-                style: {
-                  width: '80%',
-                  margin: 'auto',
-                  borderRadius: '20px'
-                }
-              }}
-            />
-          </Grid>
-        </Grid>
-        <Button variant="contained" color="primary" fullWidth>Sign Up</Button>
-      </form>
-    )}
-  </Dialog>
-
+            '&.Mui-selected': { color: '#6ecbf5' }, // 선택됐을 때의 색상
+            '&.Mui-focusVisible': {
+              backgroundColor: 'rgba(100, 100, 100, 0.2)',
+            }, // 포커스 됐을 때의 배경색
+          }}
+        />
+        <Tab
+          label='Sign Up'
+          value='signUp'
+          sx={{
+            '&.Mui-selected': { color: '#6ecbf5' }, // 선택됐을 때의 색상
+            '&.Mui-focusVisible': {
+              backgroundColor: 'rgba(100, 100, 100, 0.2)',
+            }, // 포커스 됐을 때의 배경색
+          }}
+        />
+      </Tabs>
+      {value === 'signIn' && <ModalLogin />}
+      {value === 'signUp' && <ModalJoin />}
+    </Dialog>
   );
 };
 
