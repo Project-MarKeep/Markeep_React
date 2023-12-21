e3e4f2e695a542c3a7848aa446a7d3a7f8673828
JungJaeHun <josh9269@naver.com>
2023-12-21 15:39:13 +0900

    ModalLogin.js 이메일 저장, 자동로그인, Sign in 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.scss";
-import Header from "./components/Header";
-import NaverLogin from "./components/login/NaverLogin";
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Header from './components/Header';
+import NaverLogin from './components/login/NaverLogin';
+import ModalLogin from './components/ModalLogin';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <Routes>
-        <Route path="/oauth/naver" element={<NaverLogin />} />
+        <Route
+          path='/login'
+          element={<ModalLogin />}
+        />
+
+        <Route
+          path='/oauth/naver'
+          element={<NaverLogin />}
+        />
       </Routes>
     </div>
   );
